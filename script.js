ff71c68c4237ceeb08f643d3f902679397e6474c
A.Haque <144722322+aaryah1@users.noreply.github.com>
2024-03-15 01:54:09 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function myfunction(){ document.getElementById("greet").style.color="blue";
+}
+function night(){
+  document.body.style.backgroundColor="darkblue";
+}
+function paragraph(){
+  document.getElementById("anything").style.fontFamily="Cursive";
+}
